0a12bf6bd06f20e658f4a40ef487bb24b6015733
Lies <Lies@DESKTOP-LD27GAE>
2017-09-21 14:47:26 +0200

    Demo's on csv & navigation added

---
+++ Demo Navigation/week2_Demo/week2_Demo/MainPage.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+//make beer class accessible 
+using Demo_Navigation.Model;
+
+//make Debug.WriteLine possible to write to output
+using System.Diagnostics;
+using Demo_Navigation.View;
+
+namespace Demo_Navigation
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            
+            LoadAllBeers();
+        }
+               
+
+        /// <summary>
+        /// Uses the static GetBeers method of the Beer class to:
+        /// - get a list of beers from csv file
+        /// - show the list in the associated listview
+        /// </summary>
+        private void LoadAllBeers()
+        {
+            //call static method GetBeers of Beer class
+            List<Beer> allBeers = Beer.GetBeers();
+
+            //print all beers to output window using PrintBeers method
+            lstBeers.ItemsSource = allBeers;
+        }
+
+        private void lstBeers_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            NavigateToDetails(lstBeers.SelectedItem as Beer);
+        }
+
+        private void NavigateToDetails(Beer beer)
+        {
+            if (beer != null) //was there a beer selected?
+            {
+                Navigation.PushAsync(new DetailPage(beer));
+
+                ////alternative for DetailPage IF constructor would have NO parameters:
+                //DetailPage page = new DetailPage();
+                //page.BeerContent = beer;
+                //Navigation.PushAsync(page);
+            }
+        }
+    }
+}
---
+++ Demo Navigation/week2_Demo/week2_Demo/View/DetailPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Demo_Navigation.Model;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Demo_Navigation.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DetailPage : ContentPage
+    {
+        public Beer BeerContent { get; set; }
+
+        public DetailPage(Beer beer)
+        {
+            InitializeComponent();
+
+            BeerContent = beer;
+            ShowDetails();
+        }
+
+        /// <summary>
+        /// Shows the details of this beer object in the associated labels in the GUI (see: MainPage.xaml).
+        /// </summary>
+        /// <param name="beer">The beer object whose details you wish to show.</param>
+        private void ShowDetails()
+        {
+            this.Title = BeerContent.Name;      //set the TITLE in the title bar of this page
+            lblBrewery.Text = BeerContent.Brewery;
+            lblColor.Text = BeerContent.Color;
+
+            //The text property only allows string objects,
+            //  adding a string "%" to the alcohol property automatically converts the whole thing to a string
+            lblAlcohol.Text = BeerContent.Alcohol + "%";
+        }
+
+    }
+}
